baf4389ffa0fb5a40344bc38760d3a4f632a5019
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-05-31 13:10:30 +0100

    creating commands for bot

--- brody_bot.py
+++ brody_bot.py
@@ -1,22 +1,33 @@
 #Import discord package
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
 # Load in our .env file which is a secret file containing the Discord API Token
 load_dotenv()
-
 DISCORD_TOKEN = os.getenv("DISCORD_API_KEY")
 
 # Client (our bot)
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
+
+@client.command(name='bark')
+async def bark(context):
+    await context.message.channel.send("WOOF WOOF")
+
+# @client.event
+# async def on_ready():
+#     # DO STUFF...
+#     general_channel = client.get_channel(685833302246293537) #insert channel ID
+
+#     await general_channel.send('Hello Bros!')
 
 @client.event
-async def on_ready():
-    # DO STUFF...
-    general_channel = client.get_channel(685831799880155153) #insert channel ID
-
-    await general_channel.send('Hello Bros!')
+async def on_message(message):
+    if message.content == "give me a woof":
+        general_channel = client.get_channel(685833302246293537)
+        await general_channel.send('WOOF WOOF')
+    await client.process_commands(message)
 
 # Run the client on the server
 client.run(DISCORD_TOKEN)
