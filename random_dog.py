381fd2a9996beb762ae38f0a8f38f228fb0e8820
MrSpeedy68 <mrspeedygamer123@gmail.com>
2022-06-03 22:22:53 +0100

    Finding if u sent an image in previous message for image recognizsion

--- random_dog.py
+++ random_dog.py
@@ -9,18 +9,19 @@ DISCORD_TOKEN = os.getenv("DISCORD_API_KEY")
 
 client = commands.Bot(command_prefix='!')
 
-@client.command(name='randomdog')
+@client.command(name='pic')
 async def random_dog(context):
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get("https://random.dog/woof.json") as r:
-            
-            data = await r.json()
-            embed = discord.Embed(
-                title="Doggo",
-                color = context.author.color
-            )
-            embed.set_image(url=data['url'])
-            await context.send(embed=embed)
+    channel = context.message.channel
+    #message = await channel.fetch_message(channel.last_message_id)
 
+    async for message in channel.history(limit=2):
+        if "attachments" in message.content :
+             print("Content " + message.content)
+             await context.message.channel.send(message.content)
+
+
+    #await context.send("work")
+    #await context.message.channel.send(message.content)
+    
 
 client.run(DISCORD_TOKEN)
